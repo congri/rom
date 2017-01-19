39351f40fcb2366057bc8997804aaba7e191bc57
constantin <constantin.grigo@tum.de>
2017-01-19 13:19:07 +0100

    Print warning for imaginary feature function output and ignore imaginary part

--- rom/DesignMatrix.m
+++ rom/DesignMatrix.m
@@ -92,6 +92,13 @@ classdef DesignMatrix
                 if(~all(all(all(isfinite(Phi.designMatrices{i})))))
                     warning('Non-finite design matrix Phi. Setting non-finite component to 0.')
                     Phi.designMatrices{i}(~isfinite(Phi.designMatrices{i})) = 0;
+                elseif(~all(all(isreal(Phi.designMatrices{i}))))
+                    warning('Complex feature function output:')
+                    dataPoint = i
+                    [coarseElement, featureFunction] = ind2sub(size(Phi.designMatrices{i}),...
+                        find(imag(Phi.designMatrices{i})))
+                    disp('Ignoring imaginary part...')
+                    Phi.designMatrices{i} = real(Phi.designMatrices{i});
                 end
             end
             disp('done')
