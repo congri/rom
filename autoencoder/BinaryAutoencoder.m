35ceea8cc01e869e835d67bc6c27f43e8a98effc
Constantin Grigo <constantin.grigo@tum.de>
2017-07-10 19:49:43 +0200

    Working binary autoencoder a la Tipping

--- autoencoder/BinaryAutoencoder.m
+++ autoencoder/BinaryAutoencoder.m
@@ -8,6 +8,8 @@ classdef BinaryAutoencoder
         w
         b
         xi
+        mu
+        C
     end
     
     methods
@@ -23,39 +25,17 @@ classdef BinaryAutoencoder
             %Initialize
             C = repmat(I, 1, 1, N);   %Variational Gaussian mean and cov
             mu = zeros(this.latentDim, N);  %Column vector
-            xi = ones(size(this.trainingData));   %Variational params, see Tipping
-            zzT = C;    %= <z*z^T>_tilde{p}
+            xi = zeros(size(this.trainingData));   %Variational params, see Tipping
+            lambda = this.lambda(xi);
             zzT_hat = zeros(this.latentDim + 1, this.latentDim + 1, N);
             z_hat = zeros(this.latentDim + 1, N);
-            w = zeros(this.latentDim, dim);
+            w = rand(this.latentDim, dim) - .5;
             w_hat = zeros(this.latentDim + 1, dim);
-            b = zeros(1, dim);
-
+            b = rand(1, dim) - .5;
+            
             iter = 0;
             converged = false;
             while(~converged)
-
-                for n = 1:N
-                    zzT(:, :, n) = zzT(:, :, n) + mu(:, n)*mu(:, n)';
-                    zzT_hat(1:this.latentDim, 1:this.latentDim, n) = zzT(:, :, n);
-                    zzT_hat(end, 1:this.latentDim, n) = mu(:, n)';
-                    zzT_hat(1:this.latentDim, end, n) = mu(:, n);
-                    zzT_hat(end, end, n) = 1;
-                    z_hat(:, n) = [mu(:, n); 1];
-                end
-                lambda = this.lambda(xi);
-                
-                for i = 1:dim
-                    mat = 0;
-                    vec = 0;
-                    for n = 1:N
-                        mat = mat + 2*lambda(i, n)*zzT_hat(:, :, n);
-                        vec = vec + (this.trainingData(i, n) - .5)*z_hat(:, n);
-                    end
-                    w_hat(:, i) = -mat\vec;
-                    w(:, i) = w_hat(1:this.latentDim, i);
-                    b(i) = w_hat(end, i);
-                end
                 
                 for n = 1:N
                     mat = 0;
@@ -72,8 +52,37 @@ classdef BinaryAutoencoder
                             2*b(i)*w(:, i)'*mu(:, n) + b(i)^2);
                     end
                 end
+                lambda = this.lambda(xi);
                 
-                if iter > 5
+                
+                zzT = C;    %= <z*z^T>_tilde{p}
+                for n = 1:N
+                    zzT(:, :, n) = zzT(:, :, n) + mu(:, n)*mu(:, n)';
+                    zzT_hat(1:this.latentDim, 1:this.latentDim, n) = zzT(:, :, n);
+                    zzT_hat(end, 1:this.latentDim, n) = mu(:, n)';
+                    zzT_hat(1:this.latentDim, end, n) = mu(:, n);
+                    zzT_hat(end, end, n) = 1;
+                    z_hat(:, n) = [mu(:, n); 1];
+                end
+               
+                
+                for i = 1:dim
+                    mat = 0;
+                    vec = 0;
+                    for n = 1:N
+                        mat = mat + 2*lambda(i, n)*zzT_hat(:, :, n);
+                        vec = vec + (this.trainingData(i, n) - .5)*z_hat(:, n);
+                    end
+                    w_hat(:, i) = -mat\vec;
+                    w(:, i) = w_hat(1:this.latentDim, i);
+                    b(i) = w_hat(end, i);
+                end
+                w_hat1 = w_hat(:, 1)
+                hold on;
+                plot(iter, w_hat1, 'bx')
+                drawnow
+                                
+                if iter > 20
                     converged = true;
                 end
                 iter = iter + 1
@@ -81,11 +90,24 @@ classdef BinaryAutoencoder
             this.w = w;
             this.b = b;
             this.xi = xi;
+            this.mu = mu;
+            this.C = C;
   
         end
         
         function [l] = lambda(this, xi)
-            l = (.5 - sigmoid(xi))./(2*xi);
+            %l = (.5 - sigmoid(xi))./(2*xi + eps);
+            l = (.5 - 1./(1 + exp(-xi)))./(2*xi);
+            l(xi == 0) = -.125;
+        end
+        
+        function [decodedData, contDecodedData] = decode(this)
+            
+            decodedData = heaviside(this.w'*this.mu + this.b');
+            
+            %Probability for x_i = 1
+            contDecodedData = sigmoid(this.w'*this.mu + this.b');
+            
         end
         
     end
